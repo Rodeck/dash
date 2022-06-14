6d371266314da2495f6270793e2620af5ad758cf
Redroddeck <pajak.gabriel@gmail.com>
2022-06-14 20:27:53 +0200

    Add dynami configuration for react app that can be provided from docker run command.

--- dashboard/src/components/trello/trello-service.tsx
+++ dashboard/src/components/trello/trello-service.tsx
@@ -1,12 +1,10 @@
 import axios from 'axios';
-import env from "react-dotenv";
 import {List} from './models/list';
 
 const TrelloService = {
 
     async getLists() : Promise<List[]> {
-        console.log(env);
-        let url = env.REACT_APP_TRELLO_URL!;
+        let url = (window as any).env.REACT_APP_TRELLO_URL!;
         let result = await axios.get<List[]>(url);
 
         return result.data
