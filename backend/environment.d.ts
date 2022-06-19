e9bd0abd2b5615b3a770ab73d5580a92192a3b26
Redroddeck <pajak.gabriel@gmail.com>
2022-06-19 10:50:36 +0200

    Use service account from env variable instead of file.

--- backend/environment.d.ts
+++ backend/environment.d.ts
@@ -5,6 +5,7 @@ declare global {
         PORT: string | undefined;
         TRELLO_APIKEY: string | undefined;
         TRELLO_TOKEN: string | undefined;
+        SERVICE_ACCOUNT_CONFIG: string | undefined;
       }
     }
   }
